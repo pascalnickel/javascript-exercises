0698f0ad8bf56d03d2ca3654a8022930f42e2a91
pascalnickel <117833444+pascalnickel@users.noreply.github.com>
2022-12-01 21:25:02 +0100

    Solve task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  const reverseArray = [];
 
+  for (i = string.length - 1; i >= 0; i--) {
+    reverseArray.push(string.charAt(i));
+  }
+
+  return reverseArray.join("");
 };
 
 // Do not edit below this line
