463d82280dd10f2f57674d0d0058f1279ed5fe69
pascalnickel <117833444+pascalnickel@users.noreply.github.com>
2022-12-02 11:40:42 +0100

    Solve task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
 
+  const fibonacciSeq = [0, 1];
+
+  for (i = 2; i <= number; i++) {
+    fibonacciSeq[i] = fibonacciSeq[i - 2] + fibonacciSeq[i - 1];
+  }
+
+  return fibonacciSeq[number];
 };
 
 // Do not edit below this line
