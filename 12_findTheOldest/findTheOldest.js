125ed0cf9df5a834b6ea6f08796d22e32a9858a2
pascalnickel <117833444+pascalnickel@users.noreply.github.com>
2022-12-02 12:30:02 +0100

    Solve task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
+const findTheOldest = function (people) {
+  people.forEach((person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+  });
+
+  return people.reduce((oldestPerson, person) => {
+    const highestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    const age = person.yearOfDeath - person.yearOfBirth;
+    return age > highestAge ? person : oldestPerson;
+  });
 };
 
 // Do not edit below this line
