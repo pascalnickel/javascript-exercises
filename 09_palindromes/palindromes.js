139ddaf20a1ecb1d0ef3738c3a041de865e5128c
pascalnickel <117833444+pascalnickel@users.noreply.github.com>
2022-12-02 11:23:12 +0100

    Solve task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const trimmedString = string
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replaceAll(" ", "")
+    .toLowerCase();
 
+  const reverseString = trimmedString.split("").reverse().join("");
+
+  return trimmedString === reverseString;
 };
 
+palindromes("Das ist mein,,,, test$ Satz!!.");
 // Do not edit below this line
 module.exports = palindromes;
